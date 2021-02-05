a35c024d08ccba5ee6c2d591f5c26b38971a4359
TBH-Kruger <tbh@kruger.dk>
2021-02-05 14:58:53 +0100

    Removed som code

--- script.py
+++ script.py
@@ -1,17 +1,5 @@
 import requests
-import sys
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Thomas'))
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
